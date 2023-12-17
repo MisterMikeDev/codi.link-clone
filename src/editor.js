bbca7e9b61d28a3c53e206e3871f63f55dbb3530
MisterMikeDev <diegoerr04@gmail.com>
2023-12-16 23:59:19 -0600

    :art: Styling fixes and emmet autocomplet added

--- src/editor.js
+++ src/editor.js
@@ -1,7 +1,9 @@
 import * as monaco from "monaco-editor";
+import { emmetHTML } from "emmet-monaco-es";
 import HtmlWorker from "monaco-editor/esm/vs/language/html/html.worker?worker";
 import CssWorker from "monaco-editor/esm/vs/language/css/css.worker?worker";
 import JsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker";
+import { registerAutoCompleteHTMLTag } from "./extensions/html-autocomplete";
 
 window.MonacoEnvironment = {
     getWorker(_, label) {
@@ -14,6 +16,9 @@ window.MonacoEnvironment = {
         return new JsWorker();
     }
 };
+
+emmetHTML(monaco);
+registerAutoCompleteHTMLTag(monaco);
 
 const COMMON_EDITOR_OPTIONS = {
     fontSize: 18,
